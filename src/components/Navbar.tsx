90734d8a3cf9bfb020aa5f82ec436ca46c823873
Ayush Singh Tomar <ayushtomarrudra@gmail.com>
2025-02-22 14:53:23 +0530

    SET_THEME_VIOLET

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
-import { ModeToggle } from './ui/ModeToggle';
-const Navbar = () => {
-  return (
-    <div>Navbar
-        <ModeToggle/>
-    </div>
-  )
-}
+import Link from "next/link";
+import { ModeToggle } from "./ui/ModeToggle";
+import { UserCheck } from "lucide-react";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import DashboardBtn from "./DashBoardBtn";
 
+function Navbar() {
+  return (
+    <nav className="border-b">
+      <div className="flex h-16 items-center px-4 container mx-auto">
+        {/* LEFT SIDE -LOGO */}
+        <Link
+          href="/"
+          className="flex items-center gap-2 font-semibold text-2xl mr-6 font-mono hover:opacity-80 transition-opacity"
+        >
+          <UserCheck className="size-10 text-purple-600" />
+          <span className="bg-gradient-to-r from-purple-600 to-pink-500 bg-clip-text text-transparent">
+  Crack it!
+</span>
+
+        </Link>
+
+        {/* RIGHT SIDE - ACTIONS */}
+        <SignedIn>
+          <div className="flex items-center space-x-4 ml-auto">
+            <DashboardBtn />
+            <ModeToggle />
+            <UserButton />
+          </div>
+        </SignedIn>
+      </div>
+    </nav>
+  );
+}
 export default Navbar;
